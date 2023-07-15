f63e3559a0c9e7aac9606fc6353a4c15f81e5cc4
Marisabel <mail@marisabel.nl>
2023-07-15 10:18:55 +0200

    issue #5 add taken date to created and modified date

--- src/main/java/nl/marisabel/images/ModifyCreatedDate.java
+++ src/main/java/nl/marisabel/images/ModifyCreatedDate.java
@@ -4,41 +4,27 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributeView;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileTime;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.logging.Logger;
 
 public class ModifyCreatedDate {
-
  private static final Logger log = Logger.getLogger(ModifyCreatedDate.class.getName());
 
- public void modify(File file, String dateTime) throws IOException {
-  Path filePath = file.toPath();
-  BasicFileAttributeView attributeView = Files.getFileAttributeView(filePath, BasicFileAttributeView.class);
-  BasicFileAttributes attributes = attributeView.readAttributes();
-  FileTime originalCreationTime = attributes.creationTime();
-
+ public void modify(File file, String dateTime) {
   try {
    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss");
-   Date takenDate = dateFormat.parse(dateTime);
-   log.info("TAKEN DATE: " + takenDate);
-   // Extract only the characters needed from the date string
-   SimpleDateFormat modifiedDateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+   Date newCreationDate = dateFormat.parse(dateTime);
 
-   String modifiedDate = modifiedDateFormat.format(takenDate);
+   Path filePath = file.toPath();
+   FileTime newCreationTime = FileTime.fromMillis(newCreationDate.getTime());
 
-   FileTime newCreationTime = FileTime.fromMillis(modifiedDateFormat.parse(modifiedDate).getTime());
+   Files.setAttribute(filePath, "basic:creationTime", newCreationTime);
 
-   log.info("MODIFIED DATE: " + modifiedDate);
-
-   attributeView.setTimes(newCreationTime, newCreationTime, originalCreationTime);
-
-
-
-  } catch (Exception e) {
+   log.info("Modified creation time of the file: " + file.getAbsolutePath());
+  } catch (ParseException | IOException e) {
    log.warning("Error modifying created date: " + e.getMessage());
   }
  }
