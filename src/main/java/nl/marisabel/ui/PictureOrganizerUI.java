2c592fe3b9bc4b17cf174f58e9c4e49f7b017218
Marisabel <72731147+immarisabel@users.noreply.github.com>
2023-07-19 20:46:53 +0200

    UI WIP

--- src/main/java/nl/marisabel/ui/PictureOrganizerUI.java
+++ src/main/java/nl/marisabel/ui/PictureOrganizerUI.java
@@ -1,21 +1,21 @@
-package nl.marisabel.ui;
-
-
-import nl.marisabel.images.OrganizePhotos;
+import nl.marisabel.whatsappImages.WhatsAppPhotosProcessor;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.text.ParseException;
 
 public class PictureOrganizerUI extends JFrame {
  private JTextField sourceFolderField;
  private JTextField destinationFolderField;
+ private JTextField whatsappSourceFolderField;
 
  public PictureOrganizerUI() {
   setTitle("Picture Organizer");
   setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  setSize(400, 200);
+  setSize(400, 300);
   setLocationRelativeTo(null);
 
   // Create the main panel
@@ -25,24 +25,29 @@ public class PictureOrganizerUI extends JFrame {
   mainPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
   // Create labels panel
-  JPanel labelsPanel = new JPanel(new GridLayout(2, 1, 5, 5));
+  JPanel labelsPanel = new JPanel(new GridLayout(3, 1, 5, 5));
   labelsPanel.setOpaque(false);
   JLabel sourceLabel = new JLabel("Source Folder:");
   JLabel destinationLabel = new JLabel("Destination Folder:");
+  JLabel whatsappSourceLabel = new JLabel("WhatsApp Source Folder:");
   sourceLabel.setForeground(Color.gray);
   destinationLabel.setForeground(Color.gray);
+  whatsappSourceLabel.setForeground(Color.gray);
   labelsPanel.add(sourceLabel);
   labelsPanel.add(destinationLabel);
+  labelsPanel.add(whatsappSourceLabel);
 
   // Create text fields panel
-  JPanel fieldsPanel = new JPanel(new GridLayout(2, 1, 5, 5));
+  JPanel fieldsPanel = new JPanel(new GridLayout(3, 1, 5, 5));
   fieldsPanel.setOpaque(false);
   sourceFolderField = new JTextField();
   destinationFolderField = new JTextField();
+  whatsappSourceFolderField = new JTextField();
   fieldsPanel.add(sourceFolderField);
   fieldsPanel.add(destinationFolderField);
+  fieldsPanel.add(whatsappSourceFolderField);
 
-  // Create button panel
+  // Create buttons panel
   JPanel buttonPanel = new JPanel();
   buttonPanel.setOpaque(false);
   JButton organizeButton = new JButton("Organize");
@@ -54,11 +59,22 @@ public class PictureOrganizerUI extends JFrame {
    public void actionPerformed(ActionEvent e) {
     String sourceFolder = sourceFolderField.getText();
     String destinationFolder = destinationFolderField.getText();
-    OrganizePhotos organizePhotos = new OrganizePhotos();
-    organizePhotos.organizePhotos(sourceFolder, destinationFolder);
+    organizePhotos(sourceFolder, destinationFolder);
+   }
+  });
+  JButton processWhatsAppButton = new JButton("Process WhatsApp Pictures");
+  processWhatsAppButton.setBackground(new Color(46, 204, 113));
+  processWhatsAppButton.setForeground(Color.white);
+  processWhatsAppButton.setFocusPainted(false);
+  processWhatsAppButton.addActionListener(new ActionListener() {
+   @Override
+   public void actionPerformed(ActionEvent e) {
+    String whatsappSourceFolder = whatsappSourceFolderField.getText();
+    processWhatsAppPictures(whatsappSourceFolder);
    }
   });
   buttonPanel.add(organizeButton);
+  buttonPanel.add(processWhatsAppButton);
 
   // Add components to the main panel
   mainPanel.add(labelsPanel, BorderLayout.WEST);
@@ -72,7 +88,25 @@ public class PictureOrganizerUI extends JFrame {
   add(mainPanel);
  }
 
+ private void organizePhotos(String sourceFolder, String destinationFolder) {
+  // Put your existing organizing logic here or call the OrganizePhotos class method
+  // based on your implementation.
+ }
 
+ private void processWhatsAppPictures(String sourceFolder) {
+  WhatsAppPhotosProcessor photosProcessor = new WhatsAppPhotosProcessor();
+  try {
+   photosProcessor.scanFiles(sourceFolder);
+  } catch (ParseException | IOException ex) {
+   ex.printStackTrace();
+   // Handle the exception as needed
+  }
+ }
 
-
+ public static void main(String[] args) {
+  SwingUtilities.invokeLater(() -> {
+   PictureOrganizerUI pictureOrganizerUI = new PictureOrganizerUI();
+   pictureOrganizerUI.setVisible(true);
+  });
+ }
 }
