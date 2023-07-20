7ff96224b6416854a472657d7051ae4e9e8ad2dc
Marisabel <72731147+immarisabel@users.noreply.github.com>
2023-07-20 08:49:51 +0200

    issue #8 : solved - can create a backup of files before processing
    this will be integrated automatically before we proceed to organize.

---
+++ src/main/java/nl/marisabel/backup/FolderBackup.java
@@ -0,0 +1,67 @@
+package nl.marisabel.backup;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class FolderBackup {
+
+
+    public void backUpAllFiles(String sourceFolderPath) {
+        String backupFolderPath = createBackupFolder(sourceFolderPath);
+        if (backupFolderPath != null) {
+            System.out.println("Backup created successfully at: " + backupFolderPath);
+        } else {
+            System.out.println("Backup creation failed.");
+        }
+    }
+
+    public static String createBackupFolder(String sourceFolderPath) {
+        File sourceFolder = new File(sourceFolderPath);
+        if (!sourceFolder.exists() || !sourceFolder.isDirectory()) {
+            System.err.println("Source folder does not exist or is not a directory.");
+            return null;
+        }
+
+        // Create the backup folder name with the specified format
+        String backupFolderName = sourceFolder.getName() + " BU" + new SimpleDateFormat("yyyyMMdd").format(new Date());
+
+        File backupFolder = new File(sourceFolder.getParent(), backupFolderName);
+        if (backupFolder.exists()) {
+            System.err.println("Backup folder already exists. Please choose a different name or destination.");
+            return null;
+        }
+
+        // Create the backup folder
+        if (!backupFolder.mkdir()) {
+            System.err.println("Failed to create the backup folder.");
+            return null;
+        }
+
+        try {
+            copyFolder(sourceFolder, backupFolder);
+            return backupFolder.getAbsolutePath();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void copyFolder(File originalFile, File newFile) throws IOException {
+        File[] files = originalFile.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                File newCopyOfFile = new File(newFile, file.getName());
+                if (file.isDirectory()) {
+                    newCopyOfFile.mkdir();
+                    copyFolder(file, newCopyOfFile);
+                } else {
+                    Files.copy(file.toPath(), newCopyOfFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                }
+            }
+        }
+    }
+}
